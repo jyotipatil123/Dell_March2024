244e2fbfe48ac8df6048e9657dfa18b914ef2093
jyotipatil123 <deshjyoti@yahoo.co.in>
2024-04-05 13:06:04 +0530

    c# programs

--- Program.cs
+++ Program.cs
@@ -1,85 +1,22 @@
-﻿//// c# demos 
-///* this is also comment line */
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//using System;
+namespace ConsoleApp_Dell_2024_demo2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            add obj= new add();
+            obj.addition(10, 2);
+            
+            sub obj1= new sub();
+            obj1.subtraction(10,2);
 
-//class program1
-//{
-//    static void Main(string[] args)
-//    {
-//        //Console.Write("welcome to c# ");
-//        //Console.WriteLine("welcome again");
-
-//        //// Console.Read();         //enter  same cursor position
-//        ////Console.ReadKey();      // any key on exit
-//        //Console.ReadLine();     // enter --next line
-
-
-//        //data types : int, char,long, bool, string, float, double....
-//        // int --int 16, int 32 , int 64
-
-
-
-//        //int a = 10;  
-//        //int b = 20;
-//        //Console.WriteLine("Addition result is : "+(a+b));
-//        //Console.WriteLine("Subtraction result is : " + (a - b));
-//        //Console.WriteLine("Multiplication result is : " + (a * b));
-//        //Console.WriteLine("Division result is : " + (a / b));
-//        //Console.WriteLine("Remainder result is : " + (a % b));
-
-//        //a = 100;
-//        //b = 200;
-
-//        //const double pi = 3.142;
-//        //Console.WriteLine("Pi value is : "+pi);
-
-//        //pi = 4.5787878;
-//        //Console.WriteLine("Now the pi value is : " + pi);
-
-//        //const float pi1 = 3.142222222f;
-//        //Console.WriteLine("Pi 1 value is : " + pi1);
-
-//        //pi1 = 5.22342;
-
-//        //implicit conversion
-
-//        //int a = 10; ;
-//        //long b = a;
-//        //Console.WriteLine("Long data type value is : "+b);
-
-//        ////explicit conversion
-//        //double x = 234.22;
-//        //Console.WriteLine(x);
-
-//        //int var1 = (int)x;
-//        //Console.WriteLine(var1);
-
-//        //Convert class
-
-//        Console.WriteLine("Enter first number : ");
-//        int num1 =  Convert.ToInt32(Console.ReadLine());
-
-//        Console.WriteLine("Enter second number : ");
-//        int num2 = Convert.ToInt32(Console.ReadLine());
-
-
-        
-//        // or 
-
-//        //Console.WriteLine("Enter first number : ");
-//        //int num1 = int.Parse(Console.ReadLine());
-
-//        //Console.WriteLine("Enter second number : ");
-//        //int num2 = int.Parse(Console.ReadLine());
-
-//        Console.WriteLine("Addition result is : " + (num1+num2));
-//        Console.WriteLine("Subtraction result is : " + (num1 - num2));
-//        Console.WriteLine("Multiplication result is : " + (num1 * num2));
-//        Console.WriteLine("Division result is : " + (num1 / num2));
-//        Console.WriteLine("Remainder result is : " + (num1 % num2));
-
-
-//        Console.ReadLine(); 
-//    }
-//}
+            Console.ReadLine();
+        }
+    }
+}
